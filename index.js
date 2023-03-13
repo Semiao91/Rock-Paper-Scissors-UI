123ab222d44ab4d590ec760a710b5321cabcd19f
Bernardo Semiao <berna.1991.1@gmail.com>
2023-03-14 01:48:43 +0530

    secondary commit

--- index.js
+++ index.js
@@ -1,48 +1,107 @@
 let playerPoints = 0;
 let computerScore = 0;
 let round = 0;
+const moves = ["rock", "paper", "scissors"];
+const playerPointsSpan = document.getElementById("player-points");
+const cpuPointsSpan = document.getElementById("cpu-points");
+const roundSpan = document.getElementById("round");
+const checkPlayer = document.getElementById("player-result");
+const checkCpu = document.getElementById("cpu-result");
+const restartButton = document.getElementById("restart-button");
+const playerChoice = document.getElementById("player-choice");
+const cpuChoice = document.getElementById("cpu-choice");
+const buttons = document.querySelectorAll('.button');
 
-function computerPlay() {
-  const moves = ["rock", "paper", "scissors"];
-  let random = Math.floor(Math.random() * moves.length);
-  return moves[random];
-}
+restartButton.style.display = "none";
+
+const computerPlay = () => moves[Math.floor(Math.random() * moves.length)];
 
 function playRound(playerSelection, computerSelection) {
 
+    roundSpan.textContent = `Round: ${round + 1}`;
 
-  if (playerSelection === computerSelection) {
-    alert("It's a tie!");
-  } 
-  else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    alert("You win!");
-    playerPoints++;
-  } else {
-    alert("You lose!");
-    computerScore++;
-  }
-
-  round++;
+    if (playerSelection === computerSelection) {
+      playerPoints++;
+      computerScore++;
+      playerChoice.textContent = `You chose ${playerSelection}`;
+      cpuChoice.textContent = `You chose ${computerSelection}`;
+      playerPointsSpan.textContent = `Player points: ${playerPoints}`;
+      cpuPointsSpan.textContent = `CPU points: ${computerScore}`;
+      checkCpu.textContent = "Tie";
+      checkPlayer.textContent = "Tie";
+      checkPlayer.setAttribute("class", "pop-glow");
+      checkCpu.setAttribute("class", "pop-glow");
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      playerPoints++;
+      playerChoice.textContent = `You chose ${playerSelection}`;
+      cpuChoice.textContent = `You chose ${computerSelection}`;
+      playerPointsSpan.textContent = `Player points: ${playerPoints}`;
+      checkPlayer.textContent = "Win"
+      checkPlayer.setAttribute("class", "pop-glow-win");
+      checkCpu.textContent = "Loose"
+      checkCpu.setAttribute("class", "pop-glow-loose");
+    } else {
+      computerScore++;
+      playerChoice.textContent = `You chose ${playerSelection}`;
+      cpuChoice.textContent = `You chose ${computerSelection}`;
+      cpuPointsSpan.textContent = `CPU points: ${computerScore}`;
+      checkPlayer.textContent = "Loose";
+      checkPlayer.setAttribute("class", "pop-glow-loose");
+      checkCpu.textContent = "Win"
+      checkCpu.setAttribute("class", "pop-glow-win");
+    }
+  
+    round++;
+  
+    if (playerPoints === 5 || computerScore === 5) {
+      gameOver();
+      buttons.forEach(button => button.style.display = 'none');
+      restartButton.style.display = "block";
+    }
+  
 }
 
-function game() {
-
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Choose rock, paper, or scissors: ").toLowerCase();
-    const computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    console.log("round", round, "|", "p -", playerSelection, "|", "cpu -", computerSelection);
-  }
-
+function gameOver() {
+  
   if (playerPoints > computerScore) {
-    console.log(`You win ${playerPoints}-${computerScore}!`);
+    checkPlayer.textContent = "Win"
+    checkPlayer.setAttribute("class", "pop-glow-win");
+    checkCpu.textContent = "Loose"
+    checkCpu.setAttribute("class", "pop-glow-loose");
+    roundSpan.textContent = `Player wins ${playerPoints}-${computerScore}!`;
   } else if (playerPoints < computerScore) {
-    console.log(`You lose ${playerPoints}-${computerScore}.`);
+    checkPlayer.textContent = "Loose";
+    checkPlayer.setAttribute("class", "pop-glow-loose");
+    checkCpu.textContent = "Win"
+    checkCpu.setAttribute("class", "pop-glow-win");
+    roundSpan.textContent = `CPU wins ${computerScore}-${playerPoints}!`;
   } else {
-    console.log(`It's a tie ${playerPoints}-${computerScore}.`);
+    checkPlayer.textContent = "Tie";
+    checkPlayer.setAttribute("class", "pop-glow");
+    checkCpu.textContent = "Tie"
+    checkCpu.setAttribute("class", "pop-glow");
+    roundSpan.textContent = `Its a tie${computerScore}-${playerPoints}!`;
   }
 }
+
+restartButton.onclick = () => {
+  playerPoints = 0;
+  computerScore = 0;
+  round = 0;
+  playerPointsSpan.textContent = "Player points: 0";
+  cpuPointsSpan.textContent = "CPU points: 0";
+  roundSpan.textContent = "Round: 0";
+  checkPlayer.textContent = "Player";
+  checkPlayer.setAttribute("class", "player")
+  checkCpu.textContent = "CPU";
+  checkCpu.setAttribute("class", "cpu");
+  playerChoice.textContent = "";
+  cpuChoice.textContent= "";
+  buttons.forEach(button => button.style.display = "block");
+  restartButton.style.display = "none";
+};
+
